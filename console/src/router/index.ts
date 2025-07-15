1c2db38a05cce9b122c522af9f2c6f0068a7d098
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-07-15 16:25:38 +0200

    testing

--- console/src/router/index.ts
+++ console/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FileExplorer from '@/views/FileExplorer.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/file-explorer',
+      name: 'file-explorer',
+      component: FileExplorer,
     },
     {
       path: '/about',
